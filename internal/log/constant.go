42a90fd162997ea4565ea5cacdfa34aa4589e132
cymon1997 <cybermonster82@yahoo.com>
2019-04-03 10:28:02 +0700

    added fix log"

--- internal/log/constant.go
+++ internal/log/constant.go
@@ -4,10 +4,27 @@ import (
 	"github.com/fatih/color"
 )
 
-var start = "[%s] "
-var end = ", Err: %v"
+var tagline = "[%s] "
+var errline = ", Err: %v"
+var newline = "\n"
 
-var Print = color.New(color.FgRed, color.Bold).PrintfFunc()
-var Info = color.New(color.FgRed, color.Bold).PrintfFunc()
-var Warn = color.New(color.FgRed, color.Bold).PrintfFunc()
-var Error = color.New(color.FgRed, color.Bold).PrintfFunc()
+var white = color.New(color.FgWhite)
+var green = color.New(color.FgGreen)
+var blue = color.New(color.FgBlue)
+var yellow = color.New(color.FgYellow)
+var red = color.New(color.FgRed)
+var redBold = color.New(color.FgRed, color.Bold)
+
+var print = white.PrintlnFunc()
+var info = green.PrintlnFunc()
+var debug = blue.PrintlnFunc()
+var warn = yellow.PrintlnFunc()
+var error = red.PrintlnFunc()
+var fatal = redBold.PrintlnFunc()
+
+var printf = white.PrintfFunc()
+var infof = green.PrintfFunc()
+var debugf = blue.PrintfFunc()
+var warnf = yellow.PrintfFunc()
+var errorf = red.PrintfFunc()
+var fatalf = redBold.PrintfFunc()
--- internal/log/log.go
+++ internal/log/log.go
@@ -1,35 +1,95 @@
 package log
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func setFormat(format string, tag bool, err bool) string {
+	if tag {
+		format = fmt.Sprint(tagline, format)
+	}
+	if err {
+		format = fmt.Sprint(format, errline)
+	}
+	return fmt.Sprint(format, newline)
+}
+
+func setLine(format string) string {
+	return fmt.Sprint(format, newline)
+}
+
+func Print(format string) {
+	print(format)
+}
 
 func Printf(format string, args ...interface{}) {
-	Print(format, args)
+	printf(setFormat(format, false, false), args)
 }
 
 func PrintDetail(flag string, format string, args ...interface{}) {
-	Print(fmt.Sprint(start, format, end), flag, args)
+	printf(setFormat(format, true, false), flag, args)
+}
+
+func Info(format string) {
+	info(format)
 }
 
 func Infof(format string, args ...interface{}) {
-	Info(format, args)
+	infof(setFormat(format, false, false), args)
 }
 
 func InfoDetail(flag string, format string, args ...interface{}) {
-	Info(fmt.Sprint(start, format, end), flag, args)
+	infof(setFormat(format, true, false), flag, args)
+}
+
+func Debug(format string) {
+	debug(format)
+}
+
+func Debugf(format string, args ...interface{}) {
+	debugf(setFormat(format, false, false), args)
+}
+
+func DebugDetail(flag string, format string, args ...interface{}) {
+	debugf(setFormat(format, true, false), flag, args)
+}
+
+func Warn(format string) {
+	warn(format)
 }
 
 func Warnf(format string, args ...interface{}) {
-	Warn(format, args)
+	warnf(setFormat(format, false, true), args)
 }
 
 func WarnDetail(flag string, format string, args ...interface{}) {
-	Warn(fmt.Sprint(start, format, end), flag, args)
+	warnf(setFormat(format, true, true), flag, args)
+}
+
+func Error(format string) {
+	error(format)
 }
 
 func Errorf(format string, args ...interface{}) {
-	Error(format, args)
+	errorf(setFormat(format, false, true), args)
 }
 
 func ErrorDetail(flag string, format string, args ...interface{}) {
-	Error(fmt.Sprint(start, format, end), flag, args)
+	errorf(setFormat(format, true, true), flag, args)
+}
+
+func Fatal(format string) {
+	fatal(format)
+	os.Exit(1)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	fatalf(setFormat(format, false, true), args)
+	os.Exit(1)
+}
+
+func FatalDetail(flag string, format string, args ...interface{}) {
+	fatalf(setFormat(format, true, true), flag, args)
+	os.Exit(1)
 }
